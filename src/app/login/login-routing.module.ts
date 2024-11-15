e76b5ca645d45563454e1f7693c2794c12b8c83a
EMANUEL-KRASS-DE-SOUZA <emanuelkraussdesouza@gmail.com>
2024-11-15 14:38:12 -0300

    Estilizando o home + exemplo de card publicação

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -3,13 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import path from 'path';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { HomeComponent } from '../home/home/home.component';
 
 const routes: Routes = [
   {
-    path: '', component: LoginComponent},
-
+    path: '', component: LoginComponent
+  },
   {
-    path: 'cadastro', component: CadastroComponent}
+    path: 'cadastro', component: CadastroComponent
+  }
+  ,
+  {
+    path: 'home', component: HomeComponent
+
+  }
 ];
 
 @NgModule({
