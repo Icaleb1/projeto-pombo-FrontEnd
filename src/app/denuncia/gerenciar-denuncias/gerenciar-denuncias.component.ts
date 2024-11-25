cc21c888fa6e704eaaeeafa103cad6a0225f46d5
EMANUEL-KRAUSS-DE-SOUZA <emanuelkraussdesouza@gmail.com>
2024-11-25 10:38:50 -0300

    commit

--- src/app/denuncia/gerenciar-denuncias/gerenciar-denuncias.component.ts
+++ src/app/denuncia/gerenciar-denuncias/gerenciar-denuncias.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Usuario } from '../../shared/model/Usuario';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoginService } from '../../shared/service/Login_service';
+import { UsuarioService } from '../../shared/service/Usuario_service';
 
 @Component({
   selector: 'app-gerenciar-denuncias',
@@ -8,5 +12,50 @@ import { Component } from '@angular/core';
   styleUrl: './gerenciar-denuncias.component.scss'
 })
 export class GerenciarDenunciasComponent {
+  public usuarioAutenticado: Usuario;
+  public idUsuarioAutenticado: string;
 
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private loginService: LoginService,
+    private usuarioService: UsuarioService,
+    
+  ){}
+
+  ngOnInit(): void {
+    
+    this.buscarUsuarioAutenticado();
+
+
+
+  }
+  public telaPerfil(): void{
+    this.router.navigate(['/home/usuario']);
+  }
+  public telaHome(): void{
+    this.router.navigate(['/home']);
+  }
+  public gerenciarDecunia(): void{
+    this.router.navigate(['/denuncia/gerenciar']);
+  }
+
+  private buscarUsuarioAutenticado(): void {
+
+    this.idUsuarioAutenticado = this.loginService.buscarIdUsuarioComToken();
+  
+    if (this.idUsuarioAutenticado) {
+  
+      this.usuarioService.buscarUsuarioPorId(this.idUsuarioAutenticado).subscribe(
+        (resultado) => {
+          this.usuarioAutenticado = resultado;
+        },
+        (erro) => {
+          console.error('Erro ao carregar perfil!', erro);
+        }
+      );
+    } else {
+      console.error('Usuário não autenticado ou token inválido.');
+    }
+  }
 }
